9e8e4bcc17e74c0473abae4f122a1e3c554f8792
S den Hollander <302045198@student.rocmondriaan.nl>
2023-10-06 12:55:23 +0200

    update met Cas ;)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,51 @@
 console.log("main.js loaded...")
-  
-function greet(){
-"hallo Wereld"
+//opdracht 1
+function greet() {
+    "hallo wereld"
 }
-
 console.log(greet);
+
+//opdracht 2
+function welcome() {
+    document.body.textContent = "Welkom bij ROC mondriaan"
+}
+welcome();
+
+//opdracht 3
+function calculateSUM() {
+    console.log(3 + 4);
+}
+calculateSUM();
+
+//opdracht 4
+function calculateSUM() {
+    console.log(8 / 2);
+}
+calculateSUM();
+
+//opdracht 5
+function showArrayOfNumbers() {
+    const showArrayOfNumbers1 = [2, 3, 11, 13, 17]
+    console.log(showArrayOfNumbers1)
+}
+showArrayOfNumbers();
+
+//opdracht 6
+function showArrayOfBrands() {
+
+    const showArrayOfBrands1 = ["mercedes", "volkswagen", "peugot", "tesla", "toyota"];
+    console.log(showArrayOfBrands1[0])
+    console.log(showArrayOfBrands1[1])
+    console.log(showArrayOfBrands1[2])
+    console.log(showArrayOfBrands1[3])
+    console.log(showArrayOfBrands1[4])
+}
+showArrayOfBrands();
+
+//opdracht 7
+let computer= 7
+let user = 5
+function usermoment(){
+    let bid = prompt("hoger of lager")
+}
+usermoment();
